2a48e83858125fac12f490785e96b257251e9e4b
davidski <davidski@deadheaven.com>
2016-09-10 20:23:42 -0700

    Bring EC2 keypairs under TF control.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
+# name of the EC2 CloudWatch Only role
 output "ec2_cloudwatch_role" {
   value = "${aws_iam_role.cw.name}"
 }
 
+# name of the EC2 CloudWatch+Tag modification role
 output "ec2_cloudwatch_tagger_role" {
   value = "${aws_iam_role.cw_tagger.name}"
+}
+
+# name of the default root key
+output "default_root_key" {
+  value = "${aws_key_pair.root.key_name}"
 }
